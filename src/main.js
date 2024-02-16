1419a818163e87c244c0153059ecefa086636db0
matite <matiastite1@gmail.com>
2024-02-16 16:30:00 -0500

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./css/styles.css";
+import "./css/animations.css";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
