6c14adc13ee598994683fa143dac187fada6f3a2
matite <matiastite1@gmail.com>
2024-02-16 16:59:26 -0500

    Template structure pokemon components

--- src/components/PokemonPicture.vue
+++ src/components/PokemonPicture.vue
@@ -1,7 +1,38 @@
 <template>
-  <h3>Pokemon Picture</h3>
+  <div class="pokemon-container">
+    <img
+      class="fade-in"
+      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+      alt="pokemon"
+    />
+    <img
+      class="hidden-pokemon"
+      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+      alt="pokemon"
+    />
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
+
+<style scoped>
+.pokemon-container {
+  height: 200px;
+}
+img {
+  height: 200px;
+  position: absolute;
+  width: 100vw;
+  margin-left: -50vw;
+  user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -webkit-user-drag: none;
+  -webkit-user-select: none;
+}
+.hidden-pokemon {
+  filter: brightness(0);
+}
+</style>
