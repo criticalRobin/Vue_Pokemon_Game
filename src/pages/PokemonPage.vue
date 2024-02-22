f41f70d06c4e502ba3a3286fbefa2a0dc353d3d5
matite <matiastite1@gmail.com>
2024-02-22 16:31:05 -0500

    Implemented logic to get the name of 4 random pokemons

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -9,6 +9,9 @@
 <script>
 import PokemonPicture from "@/components/PokemonPicture.vue";
 import PokemonOptions from "@/components/PokemonOptions.vue";
+import getPokemonOptions from "@/helpers/getPokemonOptions";
+
+getPokemonOptions();
 
 export default {
   components: {
