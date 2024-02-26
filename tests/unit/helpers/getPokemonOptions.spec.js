ab61dcea59329c3a03daa67d7b2538e7db14e4e1
matite <matiastite1@gmail.com>
2024-02-26 17:12:37 -0500

    Implemented test for getPokemonNames helper function

--- tests/unit/helpers/getPokemonOptions.spec.js
+++ tests/unit/helpers/getPokemonOptions.spec.js
@@ -1,4 +1,4 @@
-import { getPokemons } from "@/helpers/getPokemonOptions";
+import { getPokemons, getPokemonNames } from "@/helpers/getPokemonOptions";
 
 describe("getPokemonOptions helpers", () => {
   test("getPokemons debe retornar un arreglo de números", () => {
@@ -8,5 +8,14 @@ describe("getPokemonOptions helpers", () => {
     expect(pokemons[0]).toBe(1);
     expect(pokemons[100]).toBe(101);
     expect(pokemons[649]).toBe(650);
+  });
+
+  test("debe retornar un arreglo de 4 elementos con nombres de pokemons", async () => {
+    const pokemonNames = await getPokemonNames([1, 2, 3, 4]);
+
+    expect(pokemonNames[0].name).toBe("bulbasaur");
+    expect(pokemonNames[1].name).toBe("ivysaur");
+    expect(pokemonNames[2].name).toBe("venusaur");
+    expect(pokemonNames[3].name).toBe("charmander");
   });
 });
