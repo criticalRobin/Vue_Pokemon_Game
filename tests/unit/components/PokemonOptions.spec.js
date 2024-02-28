b84c8bbf3c085c635e0f58c184000f7205932152
matite <matiastite1@gmail.com>
2024-02-28 09:37:21 -0500

    Fixed snapshot test

--- tests/unit/components/PokemonOptions.spec.js
+++ tests/unit/components/PokemonOptions.spec.js
@@ -1,12 +1,19 @@
 import { shallowMount } from "@vue/test-utils";
 import PokemonOptions from "@/components/PokemonOptions.vue";
+import { pokemonsArr } from "../mocks/pokemons.mock";
 
 describe("pruebas unitarias componente PokemonOptions", () => {
-  test("debe hacer match con el snapshot", () => {
-    const wrapper = shallowMount(PokemonOptions, {
-      props: { pokemonsArr: [] },
-    });
+  let wrapper;
 
+  beforeEach(() => {
+    wrapper = shallowMount(PokemonOptions, {
+      props: {
+        pokemonsArr,
+      },
+    });
+  });
+
+  test("debe hacer match con el snapshot", () => {
     expect(wrapper.html()).toMatchSnapshot();
   });
 });
