e41a50260060ed42392382e2210a10fdfc091b17
matite <matiastite1@gmail.com>
2024-02-28 10:03:51 -0500

    Implemented test to check if after a click action the selection is emitted with the correct parameters

--- tests/unit/components/PokemonOptions.spec.js
+++ tests/unit/components/PokemonOptions.spec.js
@@ -26,4 +26,19 @@ describe("pruebas unitarias componente PokemonOptions", () => {
     expect(liElements[2].text()).toBe("venusaur");
     expect(liElements[3].text()).toBe("charmander");
   });
+
+  test("debe emitir selection con sus respectivos parámetros al hacer click", () => {
+    const [li1, li2, li3, li4] = wrapper.findAll("li");
+
+    li1.trigger("click");
+    li2.trigger("click");
+    li3.trigger("click");
+    li4.trigger("click");
+
+    expect(wrapper.emitted("selection").length).toBe(4);
+    expect(wrapper.emitted("selection")[0]).toStrictEqual([1]);
+    expect(wrapper.emitted("selection")[1]).toStrictEqual([2]);
+    expect(wrapper.emitted("selection")[2]).toStrictEqual([3]);
+    expect(wrapper.emitted("selection")[3]).toStrictEqual([4]);
+  });
 });
